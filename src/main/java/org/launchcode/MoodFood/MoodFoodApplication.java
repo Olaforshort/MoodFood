cf0e837c98056fd2c84ee329e0d321058ff7c4dd
Olaiere Odubo <Olaiere.eo@gmail.com>
2019-12-01 14:50:45 -0500

    Start

---
+++ src/main/java/org/launchcode/MoodFood/MoodFoodApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.MoodFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodFoodApplication.class, args);
+	}
+
+}
