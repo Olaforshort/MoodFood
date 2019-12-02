aa20927d4fb84a6062595a43c167574cf82eb126
Olaiere Odubo <olaiere.eo@gmail.com>
2019-12-02 18:41:03 -0500

    Added html files for splash page and home page

--- src/main/java/org/launchcode/MoodFood/MoodFoodApplication.java
+++ src/main/java/org/launchcode/MoodFood/MoodFoodApplication.java
@@ -3,11 +3,18 @@ package org.launchcode.MoodFood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner; // I use scanner because it's command line.
+
 @SpringBootApplication
 public class MoodFoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoodFoodApplication.class, args);
+	private void splashScreenInit() {
+		ImageIcon myImage = new ImageIcon(com.devdaily.splashscreen.SplashScreenMain.class.getResource("SplashImage.gif"));
+		screen = new SplashScreen(myImage);
+		screen.setLocationRelativeTo(null);
+		screen.setProgressMax(100);
+		screen.setScreenVisible(true);
 	}
 
-}
+
+		}
